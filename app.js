3c848ce0b5febe6ab03c1df6c78d487eb0d7ba26
Vivek <vivek.a@agiratech.com>
2023-12-29 13:22:33 +0530

    fixed a bug in which the complex expression was giving wrong answer

--- app.js
+++ app.js
@@ -1,92 +1,120 @@
-const displayArea = document.getElementById('display');
+const displayArea = document.getElementById("display");
 
-const inputArea = document.getElementById('input');
-inputArea.addEventListener('input', calculation);
+const inputArea = document.getElementById("input");
+inputArea.addEventListener("input", calculation);
 
-const clickEvent = document.querySelector('.input-buttons');
-clickEvent.addEventListener('click', calculation);
+const clickEvent = document.querySelector(".input-buttons");
+clickEvent.addEventListener("click", calculation);
 
-// updating display area if user input from mouse click
-function displayInput(nextInput){
-    inputArea.value += nextInput;
-}
 // to track next index after arithmetic symbol
-let lastCharacterIndex = 1;
+var lastCharacterIndex = 1;
 var symbolStack = [];
 var postfixExpressionList = [];
 
-function calculation(event){
-    mouseClickEvent(event);
-    insertIntopostfixExpressionList(event);
+function calculation(event) {
+  mouseClickEvent(event);
+  insertIntopostfixExpressionList(event);
 }
 
-function insertIntopostfixExpressionList(event){
-    var currentValueLength = inputArea.value.length;
-    let lastValue = inputArea.value.charAt(currentValueLength-1);
-    if(lastValue < '0' || lastValue > '9'){
-        postfixExpressionList.push(inputArea.value.substring(lastCharacterIndex-1, currentValueLength-1));
-        lastCharacterIndex = currentValueLength+1;
-        convertToPostfixExpression(lastValue);
-    } else if (event.target.id == 'equals'){
-        postfixExpressionList.push(inputArea.value.substring(lastCharacterIndex-1, currentValueLength));
-        lastCharacterIndex = currentValueLength+1;
-        emptySymbolStack();
-        displayArea.value = performCalculations();
-        resetState();
-    }
+// updating display area if user input from mouse click
+function displayInput(nextInput) {
+  inputArea.value += nextInput;
+  console.log(inputArea.value);
 }
 
-function performCalculations(){
-    let numberStack =[];
-    while(postfixExpressionList.length > 0){
-        let currentElement = postfixExpressionList.shift();
-        if(currentElement == '+' || currentElement == '-' || currentElement == '*' || currentElement == '/'){
-            let secondNumber = parseInt(numberStack.pop());
-            let firstNumber = parseInt(numberStack.pop());
-            console.log(firstNumber + " " + secondNumber);
-            if(currentElement == '+') numberStack.push(firstNumber + secondNumber);
-            else if(currentElement == '-') numberStack.push(firstNumber - secondNumber);
-            else if(currentElement == '/') numberStack.push(firstNumber / secondNumber);
-            else if(currentElement == '*') numberStack.push(firstNumber * secondNumber);
-        } else {
-            numberStack.push(currentElement);
-        }
-    }
-    console.log(postfixExpressionList);
-    return numberStack.pop();
+function insertIntopostfixExpressionList(event) {
+  var currentValueLength = inputArea.value.length;
+  var lastValue = inputArea.value.charAt(currentValueLength - 1);
+  if (lastValue < "0" || lastValue > "9") {
+    postfixExpressionList.push(
+      inputArea.value.substring(lastCharacterIndex - 1, currentValueLength - 1)
+    );
+    lastCharacterIndex = currentValueLength + 1;
+    convertToPostfixExpression(lastValue);
+  } else if (event.target.id == "equals") {
+    postfixExpressionList.push(
+      inputArea.value.substring(lastCharacterIndex - 1, currentValueLength)
+    );
+    lastCharacterIndex = currentValueLength + 1;
+    emptySymbolStack();
+    var result = performCalculations();
+    console.log(result);
+    if (isNaN(result)) result = "Malformed expression";
+    displayArea.value = result;
+  }
 }
 
-function mouseClickEvent(event){
-    let buttonValue = event.target.id;
-    console.log(buttonValue);
-    if(event.target.tagName === 'BUTTON' && buttonValue != 'reset' && buttonValue != 'equals'){
-        displayInput(buttonValue);
-    } else if (event.target.tagName === 'BUTTON' && buttonValue == 'reset'){
-        displayArea.value = '';
-        resetState();
-    }
-}
-
-function convertToPostfixExpression(lastValue){
-    if(symbolStack.length != 0){
-        if((symbolStack[symbolStack.length-1] == '-' || symbolStack[symbolStack.length-1] == '+') && (lastValue == '/' || lastValue == '*')){
-            symbolStack.push(lastValue);
-        } else {
-            postfixExpressionList.push(symbolStack.pop());
-            symbolStack.push(lastValue);
-        }
+function performCalculations() {
+  var numberStack = [];
+  while (postfixExpressionList.length > 0) {
+    var currentElement = postfixExpressionList.shift();
+    if (
+      currentElement == "+" ||
+      currentElement == "-" ||
+      currentElement == "*" ||
+      currentElement == "/"
+    ) {
+      var secondNumber = parseFloat(numberStack.pop());
+      var firstNumber = parseFloat(numberStack.pop());
+      if (currentElement == "+") numberStack.push(firstNumber + secondNumber);
+      else if (currentElement == "-")
+        numberStack.push(firstNumber - secondNumber);
+      else if (currentElement == "/")
+        numberStack.push(firstNumber / secondNumber);
+      else if (currentElement == "*")
+        numberStack.push(firstNumber * secondNumber);
     } else {
-        symbolStack.push(lastValue);
+      numberStack.push(currentElement);
     }
-
+  }
+  return numberStack.pop();
 }
 
-function emptySymbolStack(){
-    while(symbolStack.length > 0){
-        postfixExpressionList.push(symbolStack.pop());
-    }
+function mouseClickEvent(event) {
+  var buttonValue = event.target.id;
+  if (
+    event.target.tagName === "BUTTON" &&
+    buttonValue != "reset" &&
+    buttonValue != "equals"
+  ) {
+    displayInput(buttonValue);
+  } else if (event.target.tagName === "BUTTON" && buttonValue == "reset") {
+    displayArea.value = "";
+    resetState();
+  }
 }
 
-function resetState(){
-    
+function convertToPostfixExpression(lastValue) {
+  while (
+    symbolStack.length > 0 &&
+    (((symbolStack[symbolStack.length - 1] === "+" ||
+      symbolStack[symbolStack.length - 1] === "-") &&
+      (lastValue === "+" || lastValue === "-")) ||
+      ((symbolStack[symbolStack.length - 1] === "/" ||
+        symbolStack[symbolStack.length - 1] === "*") &&
+        (lastValue === "+" ||
+          lastValue === "-" ||
+          lastValue === "/" ||
+          lastValue === "*")))
+  ) {
+    var poppedValue = symbolStack.pop();
+    postfixExpressionList.push(poppedValue);
+  }
+  symbolStack.push(lastValue);
+}
+
+function emptySymbolStack() {
+  while (symbolStack.length > 0) {
+    postfixExpressionList.push(symbolStack.pop());
+  }
+}
+
+function resetState() {
+  lastCharacterIndex = 1;
+  symbolStack = [];
+  postfixExpressionList = [];
+  numberStack = [];
+  displayArea.value = "";
+  inputArea.value = "";
+  console.log();
 }
