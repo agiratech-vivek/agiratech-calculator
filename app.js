1d7e050f96457bd13ee527dfebcff20e5980144f
Vivek <vivek.a@agiratech.com>
2024-01-04 23:13:42 +0530

    few changes done

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const path = require("path");
-
 const express = require("express");
-
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -11,13 +9,13 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(request, response){
-    // const filePath = path.join(__dirname, 'views', 'index.html');
-    // response.sendFile(filePath);
-    response.render('index');
+    const filePath = path.join(__dirname, 'views', 'index.html');
+    response.sendFile(filePath);
+    //response.render('index');
 });
 
-app.post('/', function(request, response){
+// app.post('/', function(request, response){
 
-});
+// });
 
 app.listen(4000);
